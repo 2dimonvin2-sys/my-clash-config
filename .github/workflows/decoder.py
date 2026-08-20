f37c77b471310c2735fe8efe751c2239d4ca904c
2dimonvin2-sys <2dimonvin2@gmail.com>
2026-08-20 17:49:38 +0300

    Update decoder.py

--- .github/workflows/decoder.py
+++ .github/workflows/decoder.py
@@ -1,15 +1,16 @@
 import urllib.request
 import re
-import os
 from urllib.parse import urlparse, parse_qs, unquote
 
-# Сюда закидываем вообще любые базы (включая новую с Base64 и vmess)
+# Наш полный список скрытых сырых баз
 complex_raw_urls = [
-    #"https://githubusercontent.com",
+    "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt",
+    "https://raw.githubusercontent.com/anaer/Sub/main/clash.yaml",
     "https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/main/vless_configs.txt",
+    "https://raw.githubusercontent.com/FreeFolksOn/abc-configs-free-vpn-proxy-list/main/README.md",
     "https://raw.githubusercontent.com/flaafix/AetrisVPN/refs/heads/main/AetrisVPN.txt",
-    "https://raw.githubusercontent.com/bobrinaw/vlessforu/refs/heads/main/working_configs.txt",
-    #"https://vlessfo.ru"
+    "https://raw.githubusercontent.com/bobrinaw/vlessforu/refs/heads/main/working_configs.txt"
+          ]
 ]
 
 print("=== 🧪 ЗАПУСК АВТОНОМНОГО ДЕКОДЕРА БАЗ ===")
@@ -27,23 +28,24 @@ for url in complex_raw_urls:
         
         for link in found:
             try:
-                # ИСПРАВЛЕНО: Принудительно декодируем %40 в @, %2F в / прямо на лету!
+                # МГНОВЕННО очищаем ссылку от %40 (@) и %2F (/) перед любым анализом
                 clean_link = unquote(link.strip())
                 
                 parsed = urlparse(clean_link)
                 if parsed.scheme == "vless":
                     params = parse_qs(parsed.query)
-                    # Железный щит: выкидываем кривой REALITY мусор без ключа pbk
+                    # Наш железный щит против кривого REALITY мусора
                     if "sid" in params and "pbk" not in params:
                         continue
                         
                 clean_vless_links.append(clean_link)
             except:
                 continue
+    except Exception as e:
+        print(f"❌ Ошибка сети на источнике -> {e}")
 
-
-# Сохраняем чистый список локально
+# ЗАПИСЬ ФАЙЛА (строго вне цикла и на самом левом краю экрана!)
 with open("pure_raw_proxies.txt", "w", encoding="utf-8") as f:
     f.write("\n".join(clean_vless_links))
 
-print(f"📦 ДЕКОДЕР СФОРМИРОВАЛ: {len(clean_vless_links)} чистых ссылок.")
+print(f"📦 ДЕКОДЕР УСПЕШНО СФОРМИРОВАЛ: {len(clean_vless_links)} чистых ссылок без знаков % !")
